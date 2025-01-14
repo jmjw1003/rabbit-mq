ffb600f44e089348209778be91bba0a0d984029f
James Watson <jmjw1991@outlook.com>
2025-01-14 20:03:49 +0000

    Got my file names the wrong way round :<

--- queues/3_routing/emit_log_direct.py
+++ queues/3_routing/emit_log_direct.py
@@ -2,38 +2,19 @@ import pika
 import sys
 
 
-def callback(ch, method, properties, body):
-    print(f" [x] {method.routing_key}: {body}")
-
-
 def main():
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host="localhost")
-    )
+        pika.ConnectionParameters(host="localhost"))
     channel = connection.channel()
 
-    channel.exchange_declare(exchange="firect_logs", exchange_type="direct")
+    channel.exchange_declare(exchange="direct_logs", exchange_type="direct")
 
-    result = channel.queue_declare(queue="", exclusive=True)
-    queue_name = result.method.queue
-
-    severities = sys.argv[1:]
-    if not severities:
-        sys.stderr.write(f"Usage: {sys.argv[0]} [info] [warning] [error]\n")
-        sys.exit(1)
-    
-    for severity in severities:
-        channel.queue_bind(
-            exchange="direct_logs", queue=queue_name, routing_key=severity
-        )
-    
-    print(" [*] Waiting for logs. To exit press CTRL+C")
-
-    channel.basic_consume(
-        queue=queue_name, on_message_callback=callback, auto_ack=True
-    )
-
-    channel.start_consuming()
+    severity = sys.argv[1] if len(sys.argv) > 1 else "info"
+    message = " ".join(sys.argv[2:]) or "Hello World!"
+    channel.basic_publish(
+        exchange="direct_logs", routing_key=severity, body=message)
+    print(f" [x] Sent {severity}:{message}")
+    connection.close()
 
 
 if __name__ == "__main__":
